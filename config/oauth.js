84c03902828ca1115418e0911e4247945c11ae81
jaysartwork <jayalbert2022@gmail.com>
2026-03-13 09:46:19 +0800

    fix: resolve oauth.js conflict

--- config/oauth.js
+++ config/oauth.js
@@ -3,11 +3,7 @@ const { google } = require('googleapis');
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-<<<<<<< HEAD
-   process.env.GOOGLE_CALLBACK_URL
-=======
   process.env.GOOGLE_CALLBACK_URL
->>>>>>> 0cc4553a9e3a96acd13ef280a34e5e73b5b53a3f
 );
 
 const getAuthUrl = () => {
@@ -18,12 +14,8 @@ const getAuthUrl = () => {
       'https://www.googleapis.com/auth/userinfo.profile',
       'https://www.googleapis.com/auth/gmail.readonly',
       'https://www.googleapis.com/auth/gmail.modify',
-<<<<<<< HEAD
-      'https://www.googleapis.com/auth/gmail.settings.basic'  // ✅ ADDED: Para sa filter creation
-=======
       'https://www.googleapis.com/auth/gmail.settings.basic',
-      'https://www.googleapis.com/auth/gmail.send'  // ✅ IDAGDAG ITO
->>>>>>> 0cc4553a9e3a96acd13ef280a34e5e73b5b53a3f
+      'https://www.googleapis.com/auth/gmail.send'
     ],
     prompt: 'consent'
   });
