505c1723bd314697a6e600a4c4fdde9059d5a5b1
jaysartwork <jayalbert2022@gmail.com>
2026-03-13 10:59:28 +0800

    add: const { protect } = require('../middleware/auth');

--- routes/followups.js
+++ routes/followups.js
@@ -1,3 +1,4 @@
+const { protect } = require('../middleware/auth');
 const express = require('express');
 const router = express.Router();
 const { requirePremium } = require('../middleware/subscription');
